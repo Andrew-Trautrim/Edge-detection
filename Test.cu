c3f7f366864983e78ef5e73fb5edc94f2b5f8448
Andrew Trautrim <atrautrim7@gmail.com>
2026-07-02 19:09:12 -0400

    Add Gaussian kernel

--- Test.cu
+++ Test.cu
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdlib>
 #include <opencv2/opencv.hpp>
 #include <iostream>
@@ -5,14 +6,32 @@
 
 #include "Kernels.cuh"
 
+struct Kernel
+{
+    int width;
+    int height;
+    float* value;
+};
+
+Kernel construct_gaussian(int size, float sigma);
+cv::Mat apply_kernel(cv::Mat image, Kernel kernel);
 float* convolve(unsigned char* image, int image_width, int image_height, float* kernel, int kernel_width, int kernel_height);
 unsigned char* normalize(float* input, size_t size);
 
-float x_grad[3 * 3] = {
+float x_grad_data[3*3] = {
     -1, 0, 1,
     -2, 0, 2,
     -1, 0, 1
 };
+
+float y_grad_data[3*3] = {
+    -1, -2, -1,
+     0,  0,  0,
+     1,  2,  1
+};
+
+Kernel x_grad { 3, 3, x_grad_data };
+Kernel y_grad { 3, 3, y_grad_data };
 
 int main() 
 {
@@ -25,16 +44,61 @@ int main()
         return -1;
     }
 
-    float* result = convolve(image.data, image.cols, image.rows, x_grad, 3, 3);
-    unsigned char* normalized_result = normalize(result, image.cols * image.rows);
+    Kernel gaussian = construct_gaussian(31, 5.0f);
+    cv::Mat result = apply_kernel(image, gaussian);
+    cv::imwrite("../Output/gaussian.png", result);
 
+    delete gaussian.value;
+
+    // cv::Mat x_grad_image = apply_kernel(image, x_grad);
+    // cv::Mat y_grad_image = apply_kernel(image, y_grad);
+
+    // cv::imwrite("../Output/x_grad.png", x_grad_image);
+    // cv::imwrite("../Output/y_grad.png", y_grad_image);
+}
+
+// cv::Mat sobel(cv::Mat image)
+// {
+//     // 1. Calculate a and y gradients
+//     float* x_grad_result = convolve(image.data, image.cols, image.rows, x_grad.value, x_grad.width, x_grad.height);
+//     float* y_grad_result = convolve(image.data, image.cols, image.rows, y_grad.value, y_grad.width, y_grad.height);
+
+//     // 2. Calculate gradient magnitude
+
+//     // 3. Normalize result
+//     unsigned char* normalized_result = normalize(result, image.cols * image.rows);
+//     cv::Mat result_image(image.rows, image.cols, image.type(), normalized_result);
+// }
+
+Kernel construct_gaussian(int size, float sigma)
+{
+    float* kernel = new float[size * size];
+
+    const int radius = size / 2;
+    const float sigma2 = sigma * sigma;
+    const float coefficient = 1.0f / (2.0f * static_cast<float>(M_PI) * sigma2);
+
+    for (int y = -radius; y <= radius; ++y)
+    {
+        for (int x = -radius; x <= radius; ++x)
+        {
+            float value = coefficient * std::exp(-(x * x + y * y) / (2.0f * sigma2));
+            kernel[(y + radius) * size + (x + radius)] = value;
+        }
+    }
+
+    return Kernel { size, size, kernel };
+}
+
+cv::Mat apply_kernel(cv::Mat image, Kernel kernel)
+{
+    float* result = convolve(image.data, image.cols, image.rows, kernel.value, kernel.width, kernel.height);
+    unsigned char* normalized_result = normalize(result, image.cols * image.rows);
     cv::Mat result_image(image.rows, image.cols, image.type(), normalized_result);
 
-    // Write image
-    cv::imwrite("output.png", result_image);
-
     free(result);
-    free(normalized_result);
+
+    return result_image;
 }
 
 float* convolve(
